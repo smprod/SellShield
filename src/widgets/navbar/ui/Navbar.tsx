ccd131d5296968532372b2ba143f5f6ee299df79
smprod <vova230810petrov@gmail.com>
2024-01-10 01:19:31 +0300

    fix: Navbar small fix

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+'use client'
+
+import React, { useState } from "react";
 import {
     Navbar as NavbarNextUI,
     NavbarBrand,
@@ -14,49 +16,46 @@ import {menuItems} from "@shared/config";
 
 const montserrat = Montserrat({subsets: ["latin"]})
 export const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
-        <NavbarNextUI className="bg-primary data-[menu-open=true]:bg-primary-dark" disableAnimation>
-            <div>
-                <NavbarContent className="md:hidden pr-3" justify="center">
-                    <NavbarBrand>
-                        <Logo height="50px" width="50px" />
-                        <p className={`font-medium text-[20px] text-white ${montserrat.className}`}>Sell Shield</p>
-                    </NavbarBrand>
-                </NavbarContent>
+        <NavbarNextUI className="bg-primary data-[menu-open=true]:bg-primary-dark" disableAnimation onMenuOpenChange={setIsMenuOpen}>
+            <NavbarContent className="md:hidden pr-3" justify="center">
+                <NavbarBrand>
+                    <Logo height="50px" width="50px" />
+                    <p className={`font-medium text-[20px] text-white ${montserrat.className}`}>Sell Shield</p>
+                </NavbarBrand>
+            </NavbarContent>
 
-                <NavbarContent className="hidden md:flex gap-[2vw]" justify="center">
-                    <NavbarBrand>
-                        <Logo height="50px" width="50px" />
-                        <p className={`font-medium text-[20px] text-white ${montserrat.className}`}>Sell Shield</p>
-                    </NavbarBrand>
-                    {menuItems.map((item, index) => (
-                        <NavbarItem key={`${item.title}-${index}`}>
-                            <Link
-                                className="w-full text-white"
-                                href={item.href}
-                                size="lg"
-                            >
-                                {item.title}
-                            </Link>
-                        </NavbarItem>
-                    ))}
-                </NavbarContent>
-            </div>
-
-            <div>
-                <NavbarContent className="hidden md:flex" justify="end">
-                    <NavbarItem>
-                        <div className="flex gap-[1.5vw] items-center">
-                            <SignUpButton/>
-                            <LogInButton/>
-                        </div>
+            <NavbarContent className="hidden md:flex gap-[2vw]" justify="center">
+                <NavbarBrand>
+                    <Logo height="50px" width="50px" />
+                    <p className={`font-medium text-[20px] text-white ${montserrat.className}`}>Sell Shield</p>
+                </NavbarBrand>
+                {menuItems.map((item, index) => (
+                    <NavbarItem key={`${item.title}-${index}`}>
+                        <Link
+                            className="w-full text-white"
+                            href={item.href}
+                            size="lg"
+                        >
+                            {item.title}
+                        </Link>
                     </NavbarItem>
-                </NavbarContent>
+                ))}
+            </NavbarContent>
 
-                <NavbarContent className="md:hidden" justify="end">
-                    <NavbarMenuToggle className="text-white" />
-                </NavbarContent>
-            </div>
+            <NavbarContent className="hidden md:flex" justify="end">
+                <NavbarItem>
+                    <div className="flex gap-[1.5vw] items-center">
+                        <SignUpButton/>
+                        <LogInButton/>
+                    </div>
+                </NavbarItem>
+            </NavbarContent>
+
+            <NavbarContent className="md:hidden" justify="end">
+                <NavbarMenuToggle className="text-white" aria-label={isMenuOpen ? "Close menu" : "Open menu"}/>
+            </NavbarContent>
 
             <NavbarMenu className="bg-primary-dark">
                 {menuItems.map((item, index) => (
