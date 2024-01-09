e7e072d9e69baab8df907181a48be02b71917907
smprod <vova230810petrov@gmail.com>
2024-01-10 01:18:41 +0300

    feat: HomeInfoCard

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,10 +1,12 @@
 import {BaseLayout} from "@app";
+import {HomeInfoCard} from "@widgets/home";
 
 export const HomePage = () => {
     return (
         <BaseLayout>
-            <>
-            </>
+            <div>
+                <HomeInfoCard/>
+            </div>
         </BaseLayout>
     )
 }
---
+++ src/widgets/home/ui/HomeInfoCard.tsx
@@ -0,0 +1,22 @@
+import {Card, CardBody, CardFooter, CardHeader} from "@nextui-org/react";
+import {SignUpButton} from "@features/account";
+
+export const HomeInfoCard = () => {
+    return (
+        <Card className="max-w-[35.5vw] bg-transparent shadow-none my-10 mx-[7.5vw]">
+            <CardHeader className="flex gap-3">
+                <p className="text-[49px] text-white">
+                    Ваше спокойствие — наше обещание.
+                </p>
+            </CardHeader>
+            <CardBody>
+                <p className="text-white opacity-60">
+                    Sell Shield: ваш надежный союзник в обеспечении безопасности и минимизации финансовых рисков при онлайн-сделках.
+                </p>
+            </CardBody>
+            <CardFooter>
+                <SignUpButton/>
+            </CardFooter>
+        </Card>
+    )
+}
